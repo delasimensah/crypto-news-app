ff86523f4a9103140dc60a9f02a150b7cfeb7617
delasimensah <mensadelasi@gmail.com>
2021-10-25 12:56:40 +0000

    initial setup added navbar component, page components and setup redux

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import { Switch, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+
+//components
+import { Navbar } from "./components";
+
+//pages
+import Homepage from "./pages/Homepage";
+import Exchanges from "./pages/Exchanges";
+import Cryptocurrencies from "./pages/Cryptocurrencies";
+import CryptoDetails from "./pages/CryptoDetails";
+import News from "./pages/News";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/">
+                <Homepage />
+              </Route>
+              <Route exact path="/exchanges">
+                <Exchanges />
+              </Route>
+              <Route exact path="/cryptocurrencies">
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path="/crypto/:coinId">
+                <CryptoDetails />
+              </Route>
+              <Route exact path="/news">
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
+
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Copyright © 2021
+            <Link to="/"> Cryptonews Inc.</Link> <br />
+            All Rights Reserved.
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
